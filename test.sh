fa384170a410eec3040e8a3884e04755fec412b0
concave-dash <kwerkee12@gmail.com>
2022-05-09 23:37:44 -0400

    test

--- test.sh
+++ test.sh
@@ -1,19 +1,20 @@
 /home/hipi/Sites/GooDee/_airflow/exit.sh
 status=$?
 if [ $status -ne 0 ]; then
-    piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
+    #piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
     sleep 30s
     /home/hipi/Sites/GooDee/_airflow/exit.sh
 fi
 status=$?
 if [ $status -ne 0 ]; then
-    piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
+    #piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
     sleep 30s
     /home/hipi/Sites/GooDee/_airflow/exit-good.sh
 fi
 status=$?
 if [ $status -ne 0 ]; then
-    piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
+    #piactl get regions | sort | grep us- | shuf | head -1 | xargs piactl set region >>  /home/hipi/Sites/GooDee/_airflow/pia.log
     sleep 30s
     /home/hipi/Sites/GooDee/_airflow/exit.sh
 fi
+echo $#
