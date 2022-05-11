49fba4b2cddc82861a6390e958f434d786c1a196
concave-dash <kwerkee12@gmail.com>
2022-05-11 00:40:42 -0400

    update 2 commands

---
+++ get_recent_collections.sh
@@ -0,0 +1,1 @@
+../opensea-monitor/start-recent.sh
---
+++ upcoming_whitelists_scraper.sh
@@ -0,0 +1,5 @@
+node /home/hipi/Sites/GooDee/nft-scraper/scrape_whitelists.js | ts '[%Y-%m-%d %H:%M:%.S]' >> /home/hipi/Sites/GooDee/_airflow/upcoming_whitelists_scraper.log
+## get status ##
+# status=$?
+## take some decision ## 
+# [ $status -eq 0 ] && echo "etherscan scrape was successful" || ssmtp kwerkee12@gmail.com < email.txt 
